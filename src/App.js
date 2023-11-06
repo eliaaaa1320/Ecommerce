3040d27da512318f069ee049766983befa76705c
Eliana Zapata Alzate <elianazapataalzate@MacBook-Air-de-Eliana.local>
2023-11-06 16:06:04 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CarritoCompras from './Carrito/carro';
 import Impresos from './Productos/producto';
 import Banner1 from './Banner/banner1';
 import { useState } from "react";
+import Footer from './Footer/footer';
 
 function App() {
   const [items, useItems] = useState ([]);
@@ -13,6 +14,7 @@ function App() {
     <Banner1></Banner1>
     <Impresos cambio={useItems}></Impresos>
     <CarritoCompras ProductoSeleccionado={items}></CarritoCompras>
+    <Footer></Footer>
     </div>
   );
 }
